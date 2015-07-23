e880558516f362003d105e34d1d85bfad2f8778f
Hiro Asari <asari.ruby@gmail.com>
2015-07-23 14:45:57 -0400

    Add dividers for prettier output

--- add_all.rb
+++ add_all.rb
@@ -5,6 +5,7 @@ require 'json'
 sources = JSON.parse(File.read(File.join(File.dirname($0), 'ubuntu.json')))
 
 successes = sources.select do |src|
+  puts "-------------------\nAdding #{src.inspect}\n"
   if src['key_url']
     system("curl -sSL #{src['key_url'].untaint.inspect} | sudo -E apt-key add -") && system("sudo -E apt-add-repository -y #{src['sourceline'].untaint.inspect}")
   else
