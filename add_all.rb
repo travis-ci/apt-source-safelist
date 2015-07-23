d4f7cd4816d1131ef4bb5aeec96d88779554d221
Hiro Asari <asari.ruby@gmail.com>
2015-07-23 14:44:12 -0400

    For CI, actually test that the sources can be added

---
+++ add_all.rb
@@ -0,0 +1,15 @@
+#! /usr/bin/env ruby
+
+require 'json'
+
+sources = JSON.parse(File.read(File.join(File.dirname($0), 'ubuntu.json')))
+
+successes = sources.select do |src|
+  if src['key_url']
+    system("curl -sSL #{src['key_url'].untaint.inspect} | sudo -E apt-key add -") && system("sudo -E apt-add-repository -y #{src['sourceline'].untaint.inspect}")
+  else
+    system("sudo -E apt-add-repository -y #{src['sourceline'].untaint.inspect}")
+  end
+end
+
+exit sources.length == successes.length
